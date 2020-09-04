34721416fedc89f3b0738b8f736956fe3562a9fb
Shah Rukh Khan <shah@crowdbotics.com>
2020-09-04 04:33:42 +0000

    Update models

--- food_app_35/wsgi.py
+++ food_app_35/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'food_app_35.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "food_app_35.settings")
 
 application = get_wsgi_application()
